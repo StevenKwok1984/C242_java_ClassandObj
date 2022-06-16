dd85aeaf23ae019abce99ba0df1c65480e2c9e6b
Steven Kwok <derekkwok2020@gmail.com>
2022-06-16 16:21:01 +0100

    wew

---
+++ CarsPac/Main.java
@@ -0,0 +1,12 @@
+package CarsPac;
+
+public class Main {
+    public static void main(String args[]) {
+        int a = 15;
+        int b = 25;
+        if ((a < b) || (a = 5) > 15)
+            System.out.println(a);
+        else
+            System.out.println(b);
+    }
+}
---
+++ CarsPac/Test.java
@@ -0,0 +1,36 @@
+package CarsPac;
+
+public class Test {
+    /*
+    public static void main(String args[]) {
+        int i = 0, j = 5;
+        for (; (i < 3) && (j++ < 10); i++) {
+            System.out.print(" " + i + " " + j);
+        }
+        System.out.print(" " + i + " " + j);
+    }
+
+
+    public static void main(String args[]) {
+        int i = 0, j = 2;
+        do {
+            i = ++i;
+            j--;
+        } while (j > 0);
+        System.out.println(i);
+    }
+
+
+     */
+    public static void main(String args[]) {
+        int x = 3, y = 5, z = 10;
+
+        int object = 0;
+
+        int[] ia = new int[15];
+        char[] ca = "some string";
+
+        System.out.println(++z +z + x++ + y - y);
+    }
+}
+
