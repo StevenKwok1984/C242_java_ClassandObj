60d29be65ed9c45347e7f5c571a74e0de9b84eff
Steven Kwok <derekkwok2020@gmail.com>
2022-06-17 15:47:59 +0100

    Create SimpleMath.java
    lecture is completed

---
+++ SimpleMath.java
@@ -0,0 +1,13 @@
+public class SimpleMath {
+
+    public static final double PI = 3.14;
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+}
