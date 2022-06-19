8c52402d1862b2995a97d0a097f88d48bc890f9c
Steven Kwok <derekkwok2020@gmail.com>
2022-06-19 16:40:03 +0100

    Finish Friday class

---
+++ Quiz/App.java
@@ -0,0 +1,27 @@
+package Quiz;
+
+public class App {
+    private Candy chocolatBar;
+    private String llamaName;
+
+
+    public String fileName;
+    // case
+
+    public String filename;
+    public Candy getChocolatBar() {
+        return chocolatBar;
+    }
+
+    public void setLlamaName(String llamaName) {
+        this.llamaName = llamaName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+}
