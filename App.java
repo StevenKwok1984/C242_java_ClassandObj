56dd1fb9fb18163dd1cb5c7e9af14f3531cf37dd
Steven Kwok <derekkwok2020@gmail.com>
2022-06-19 17:23:21 +0100

    Sunday study

--- App.java
+++ App.java
@@ -2,13 +2,22 @@ public class App {
 
     public static void main(String[] args) {
 
+        /*
         Dog myDog = new Dog();
         myDog.setName("Spot");
         myDog.setWeight(34.0);
 
         Dog anotherDog = new Dog("Buster", 23.5);
+         */
 
+        // References, Storage, and Memory Management
+        Person p = new Person();
+        Person p2 = p;
 
+        p.setName("Mary");
+        p2.setAge(19);
 
+        System.out.println(p.getAge()); // prints 19
+        System.out.println(p2.getName()); // prints Mary
     }
 }
--- test.java
+++ test.java
@@ -1,5 +1,11 @@
 public class test {
-    public static void main(String[] args){
 
+    public static void method(int parameter) {
+        int x = 5;
+        int y = parameter;
+    }
+
+    public static void main(String[] args){
+        method(5);
     }
 }
