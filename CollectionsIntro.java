023b5842df09ffe4fa59fd81928afdec6913317a
Steven Kwok <derekkwok2020@gmail.com>
2022-06-21 11:48:59 +0100

    Create CollectionsIntro.java

---
+++ CollectionsIntro.java
@@ -0,0 +1,17 @@
+import CarsPac.Car;
+import java.util.ArrayList;
+
+
+public class CollectionsIntro {
+    public static void main(String[] args) {
+        CollectionsIntro ci = new CollectionsIntro();
+        ci.arrays();
+        ci.list();
+    }
+
+    private void arrays() {
+        int[] ints1 = new int[] {3, 55, 99, -7};
+        int[] ints2 = new int[6];
+        double doubles[] = new double[]{3, 55, 99, -7};y
+    }
+}
