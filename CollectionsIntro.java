d19c20f86a93d22124515c4de70b10cc672cf914
Steven Kwok <derekkwok2020@gmail.com>
2022-06-21 11:59:45 +0100

    cghbdsghtgttrgs

--- CollectionsIntro.java
+++ CollectionsIntro.java
@@ -1,5 +1,6 @@
 import CarsPac.Car;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class CollectionsIntro {
@@ -9,9 +10,14 @@ public class CollectionsIntro {
         ci.list();
     }
 
+    private void list() {
+        List<Integer> intList1 = new ArrayList<>();
+        List<Integer> a = new ArratList<>();
+    }
     private void arrays() {
         int[] ints1 = new int[] {3, 55, 99, -7};
         int[] ints2 = new int[6];
-        double doubles[] = new double[]{3, 55, 99, -7};y
+        double doubles[] = new double[]{3, 55, 99, -7};
+
     }
 }
